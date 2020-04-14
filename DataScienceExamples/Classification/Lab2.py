343c7c69010cd76be2239cffe9fe401d6ccc351e
Oleg11_508 <oleg_o97@mail.ru>
2020-04-14 19:08:02 +0300

    lab 1 and lab 2 (clasterization, classification)

--- DataScienceExamples/Classification/Lab2.py
+++ DataScienceExamples/Classification/Lab2.py
@@ -62,6 +62,12 @@ def makeVotes(electors,X_test,Y_test):
         i+=1
     return metrics.accuracy_score(Y_test,Xp)
 
+def estimatePrediction(clf,Xt,Yt,X_test,labels):
+    clf.fit(Xt,Yt)
+    Ans = clf.predict(X_test)
+    a = metrics.accuracy_score(labels,Ans)
+    return a
+
 # read data and extract X, y.
 X = pd.read_csv('datasets/train.csv') # classes: [0,1,2,3]. column: price_range
 print('Features: '+str(len(X.columns)))
@@ -78,35 +84,41 @@ mms.fit(X)
 X = pd.DataFrame(mms.transform(X),columns=X.columns)
 
 
-# X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.3, random_state=1) # 70% training and 30% test
-electors = randomForest(X,target,15,0.7,0)
+X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.3, random_state=1) # 70% training and 30% test
+electors = randomForest(X,target,10,0.3,0)
 score1 = 0
 for t in electors:
     print(t[1])
     score1+=t[1]
-score1 = score1/15
+score1 = score1/len(electors)
 
-a = makeVotes(electors,X,target)
+a = makeVotes(electors,X_test,y_test)
 
-
-
-print('Accuracy: '+str(a))
+tree = DecisionTreeClassifier(criterion='entropy',random_state=0)
+tree.fit(X_train,y_train)
+a0 = metrics.accuracy_score(y_test,tree.predict(X_test))
+print('Single tree accuracy: '+str(a0))
+print('Accuracy of random forest: '+str(a))
 print('Mean(score): '+str(score1))
 print('\n')
 print('Libraries methods:\n')
 clf0 = DecisionTreeClassifier(criterion='entropy', random_state=0)
-scores = cross_val_score(clf0,X,target,scoring='accuracy', cv=5)
+scores = cross_val_score(clf0,X,target,scoring='accuracy', cv=10)
 print("Decision Tree: "+str(scores.mean()))
 clf1 = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
-scores = cross_val_score(clf1,X,target, scoring='accuracy', cv=5)
+scores = cross_val_score(clf1,X,target, scoring='accuracy', cv=10)
 print("Random forest: "+str(scores.mean()))
 clf2 = ExtraTreesClassifier(n_estimators=100, max_depth=None,min_samples_split=2, random_state=0)
-scores = cross_val_score(clf2, X, target, scoring='accuracy', cv=5)
+scores = cross_val_score(clf2, X, target, scoring='accuracy', cv=10)
 print("ExtraTree: "+str(scores.mean()))
 clf3 = AdaBoostClassifier(n_estimators=25)
-scores = cross_val_score(clf3,X,target, scoring='accuracy', cv=5)
+scores = cross_val_score(clf3,X,target, scoring='accuracy', cv=10)
 print('AdaBoost: '+str(scores.mean()))
+
+
+
 eclf = VotingClassifier( estimators=[('dt',clf0),('rf', clf1), ('et', clf2), ('adb', clf3)],voting='hard')
 for clf, label in zip([clf0,clf1, clf2, clf3, eclf], ['Decision Tree','Random Forest', 'Extra tree', 'AdaBoost', 'Ensemble']):
-    scores = cross_val_score(clf, X, target, scoring='accuracy', cv=5)
+    scores = cross_val_score(clf, X, target, scoring='accuracy', cv=10)
     print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+    print('Accurancy_score: '+str(estimatePrediction(clf,X_train,y_train,X_test,y_test))+' :: '+label)
